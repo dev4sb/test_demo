5866e192358511a4c44ddf4cceeb1ac1f30c8ae2
Kishan Surani <dev4sb.test@gmail.com>
2023-04-11 17:58:06 +0530

    Curl fetch Data

---
+++ curl.php
@@ -0,0 +1,23 @@
+<?php
+
+$url = "https://dog.ceo/api/breeds/image/random";
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+
+$data = json_decode($response);
+
+if(curl_errno($curl)){
+    echo "Error fetching data from Dog API: ". curl_errno($curl);
+}else{
+    ?>
+    <img src="<?php echo $data->message; ?>" alt="">
+    <?php
+}
+?>
+
